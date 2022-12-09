71a45f4057ab5c994f3fbe0e8f187a5105fe4a87
fabber4christ@gmail.com <fabber4christ@gmail.com>
2022-12-08 23:50:51 -0500

    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,10 @@
-import { StyleSheet } from "react-native";
-
 export const globalOptions = {
   name: "sambachat_37846",
   url: "https://sambachat_37846.botics.co",
   api: "https://sambachat_37846.botics.co/api/v1"
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     "copy": "Routes available!"
   }
-}
+};
